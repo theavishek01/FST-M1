f04e06c26a4bbe27e323e499f1a07a829cd9c13d
Avishek Chakraborty <theavishek01@gmail.com>
2024-08-02 14:08:25 +0530

    Act4

---
+++ TestNG/Activities/Activity4_demo2.java
@@ -0,0 +1,10 @@
+package TestNg;
+
+import org.testng.annotations.Test;
+
+public class Activity4_demo2 {
+    @Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+}
